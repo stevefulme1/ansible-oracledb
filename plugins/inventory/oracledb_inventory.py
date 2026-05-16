1b155195832a0ebadfa6429b1187b0e6bacea0a7
sfulmer <sfulmer@redhat.com>
2026-05-16 13:08:43 -0400

    Fix unused variable in inventory plugin

--- plugins/inventory/oracledb_inventory.py
+++ plugins/inventory/oracledb_inventory.py
@@ -82,7 +82,7 @@ class InventoryModule(BaseInventoryPlugin):
 
         host = self.get_option("host")
         api_key = self.get_option("api_key")
-        group = self.inventory.add_group("oracledb")
+        self.inventory.add_group("oracledb")
 
         # Discovery logic - connect to API and populate inventory
         try:
